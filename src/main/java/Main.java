9deac75f959df2775fe54f0b2ae56ddd3607e70c
adam.beresford <adam56.beresford@gmail.com>
2019-11-26 15:43:25 +0000

    added game class, testing, attacking function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import models.Game;
 import models.Model;
 import models.Player;
 import models.Sql2oModel;
@@ -15,6 +16,10 @@ import static spark.Spark.*;
 import static spark.Spark.post;
 
 public class Main {
+
+    public static Player player = new Player("Adam", 100,10,20,"true", 0 );
+    public static Player enemy = new Player("Ork", 80,20,10,"true", 0  );
+    public static Game game = new Game(player, enemy);
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
@@ -52,14 +57,19 @@ public class Main {
         }, new VelocityTemplateEngine());
 
         get("/battle", (req, res) ->{
-            Player player = new Player("Adam", 100,10,20,"true", 0 );
             HashMap battle = new HashMap();
             battle.put("player", player);
-            System.out.println(battle.get("player"));
-            System.out.println(Math.round(Math.floor(player.random_damage())));
+            battle.put("enemy", enemy);
+            // System.out.println(battle.get("player"));
+            // System.out.println(Math.round(Math.floor(game.random_damage())));
             return new ModelAndView(battle, "templates/battle.vtl");
-
         }, new VelocityTemplateEngine());
+
+        post("/attack", (req, res) ->{
+            game.attack(enemy);
+            res.redirect("/battle");
+            return null;
+        });
 
         post("/sign_out", (req, res) ->{
             req.session().attribute("Signed_In?", "false");
