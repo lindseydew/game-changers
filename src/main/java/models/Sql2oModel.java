36333ea460d62adc654b438c6c97616b8024de15
vikash <zero_transfer@outlook.com>
2019-11-26 09:19:00 +0000

    sign in and sign up, rough draft

--- src/main/java/models/Sql2oModel.java
+++ src/main/java/models/Sql2oModel.java
@@ -27,4 +27,45 @@ public class Sql2oModel implements Model {
         return null;
     }
 
+    @Override
+    public boolean UsernameExist(String username) {
+        boolean does_username_exists = false;
+        try (Connection conn = sql2o.open()) {
+            List<Users> user1 = conn.createQuery("select username from users")
+                    .executeAndFetch(Users.class);
+            String user = user1.toString();
+            if (user.contains(username)) {
+                does_username_exists = true;
+            }
+        }
+        return does_username_exists;
+    }
+
+    @Override
+    public boolean CorrectPassword(String username, String password) {
+        boolean correct_password = false;
+
+        try (Connection conn = sql2o.open()) {
+            List<Users> user = conn.createQuery("select password from users where username=:username")
+                    .addParameter("username", username)
+                    .executeAndFetch(Users.class);
+            password = "[Users(username=null, full_name=null, password=" + password + ")]";
+            if (user.toString().equals(password)) {
+                correct_password = true;
+            }
+        }
+        return correct_password;
+    }
+
+    @Override
+    public void createUser(String username, String full_name, String password) {
+        try (Connection conn = sql2o.beginTransaction()) {
+            conn.createQuery("insert into users(username, full_name, password) VALUES (:username, :full_name, :password)")
+                    .addParameter("username", username)
+                    .addParameter("full_name", full_name)
+                    .addParameter("password", password)
+                    .executeUpdate();
+            conn.commit();
+        }
+    }
 }
