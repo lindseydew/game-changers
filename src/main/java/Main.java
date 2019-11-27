9540919b068f212c1d162848690cfd53a22c94fc
adam.beresford <adam56.beresford@gmail.com>
2019-11-27 11:57:50 +0000

    added character images

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
 
     public static Player player = new Player("Adam", 100,10,20,"true", 0 );
-    public static Player enemy = new Player("Ork", 80,20,10,"true", 0  );
+    public static Player enemy = new Player("Ork", 80,5,10,"true", 0  );
     public static Game game = new Game(player, enemy);
 
     public static void main(String[] args) {
